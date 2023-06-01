9139839a05b007d4d6a8a049edc08c52625432de
Andrey Bizhov <bijev14@yandex.ru>
2023-06-01 13:51:19 +0300

    add cron

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ScheduleModule.forRoot(), HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,38 @@
+import { HttpService } from '@nestjs/axios';
+import { catchError, firstValueFrom } from 'rxjs';
 import { Injectable } from '@nestjs/common';
+import { Interval } from '@nestjs/schedule';
+import { AxiosError } from 'axios';
+
+interface CurrencyRates {
+  success: boolean;
+  timestamp: number;
+  base: string;
+  date: string;
+  rates: Record<string, number>;
+}
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  // private readonly logger = new Logger(AppService.name);
+  constructor(private readonly httpService: HttpService) {}
+
+  @Interval(15000)
+  async findAll(): Promise<any> {
+    const { data } = await firstValueFrom(
+      this.httpService
+        .get<CurrencyRates>(
+          'http://api.exchangeratesapi.io/v1/latest?access_key=cd0c2bb285addfc267142c7253a55ce2',
+        )
+        .pipe(
+          catchError((error: AxiosError) => {
+            console.log('Ошибка запроса: ', error.response.data);
+            throw 'An error happened!';
+          }),
+        ),
+    );
+
+    console.log(data);
+    return;
   }
 }
